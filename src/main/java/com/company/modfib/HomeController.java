55faa71f5790063d199615a27bd38815c842c39b
Saniya Godil <saniyagodil@gmail.com>
2018-01-29 12:34:45 -0500

    Prints only the result of modified fib

--- src/main/java/com/company/modfib/HomeController.java
+++ src/main/java/com/company/modfib/HomeController.java
@@ -15,35 +15,52 @@ public class HomeController {
 
 
     public int[] memo;
+    //public String memostring;
+
     @RequestMapping("/processform")
     public String loadFromPage(@RequestParam("newnumber") int newnumber, Model model) {
         model.addAttribute("inputval", newnumber);
 
-        memo[0] = 0;
-        memo[1] = 1;
-        memo[2] = 1;
-        memo[3] = 2;
-        memo[4] = 4;
+        //memo = new int[newnumber + 1];
+        //for (int x = 0; x <= newnumber + 1; x++){
+        //    memo[x] = -1;
+        //}
+        //memo[0] = 0;
+        //memo[1] = 1;
+        //memo[2] = 1;
+        //memo[3] = 2;
+        //memo[4] = 4;
+
         int y = Fibonnaci(newnumber);
+
+        //memostring = switchArray(memo, newnumber);
         model.addAttribute("Fibonnaci", y);
-        model.addAttribute("Sequence", memo.toString());
+        //model.addAttribute("Sequence", memostring);
         return "confirm";
     }
+
     public int Fibonnaci(int x){
-        if (x <= 0) {
+        if (x == 0) {
             return 0;
         } else if (x == 1 || x == 2) {
             return 1;
         } else if (x == 3) {
             return 2;
-        } else try{
-            return (memo[x]);
-        } catch(IndexOutOfBoundsException e){
-            memo[x] = Fibonnaci(x - 1) + Fibonnaci(x - 2) + Fibonnaci(x - 3) + Fibonnaci(x - 4);
+        } else if(x >= 4){
+
             return Fibonnaci(x - 1) + Fibonnaci(x - 2) + Fibonnaci(x - 3) + Fibonnaci(x - 4);
         }
+        return -1;
     }
-}
+
+    /*public String switchArray(int[] memo, int n){
+    *    for(int x = 0; x <= n + 1; x++){
+    *        memostring += Integer.toString(x) + "   ";
+    *    }
+    *    return memostring;
+    *    }
+    */}
+
 
 
 
